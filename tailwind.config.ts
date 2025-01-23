2ade1e58757cd025fb32e500bba8b1fa49c151a3
ghbishal <bishalk2056@gmail.com>
2025-01-23 23:11:12 +0900

    chore: button and links

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 import { nextui } from '@nextui-org/theme';
 
 const config: Config = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './node_modules/@nextui-org/theme/dist/components/(button|link).js'],
   theme: {
     screens: {
       'sm': '640px',
